ff683ac14f6305bf1f63fed9ea76b15183c56868
hassan8118 <mujtabahassan1234567@gmail.com>
2023-05-07 13:46:02 -0400

    WeeklyChallenges4Done

--- ChallengesWithTestsMark8/ChallengesSet04.cs
+++ ChallengesWithTestsMark8/ChallengesSet04.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace ChallengesWithTestsMark8
 {
@@ -6,47 +7,92 @@ namespace ChallengesWithTestsMark8
     {
         public int AddEvenSubtractOdd(int[] numbers)
         {
-            throw new NotImplementedException();
+            int sum = 0;
+            sum += numbers.Where(x => x % 2 == 0).Sum();
+            int oddsum = numbers.Where(x => x % 2 != 0).Sum();
+            return sum - oddsum;
         }
 
         public int GetLengthOfShortestString(string str1, string str2, string str3, string str4)
         {
-            throw new NotImplementedException();
+            string[] array = new string[] { str1, str2, str3, str4 };
+            int shortest = str1.Length;
+            foreach(string s in array)
+            {
+                if(s.Length < shortest)
+                {
+                    shortest = s.Length;
+                }
+            }
+            return shortest;
         }
 
         public int GetSmallestNumber(int number1, int number2, int number3, int number4)
         {
-            throw new NotImplementedException();
+            int[] array = new int[] { number1, number2, number3, number4 };
+            int smallest = number1;
+            foreach (int i in array)
+            {
+                if (i < smallest)
+                {
+                    smallest = i;
+                }
+            }
+            return smallest;
         }
 
         public void ChangeBusinessNameTo_TrueCoders(Business biz)
         {
-            throw new NotImplementedException();
+            biz.Name = "TrueCoders"; 
         }
 
         public bool CouldFormTriangle(int sideLength1, int sideLength2, int sideLength3)
         {
-            throw new NotImplementedException();
+            return sideLength1 + sideLength2 > sideLength3 && sideLength2 + sideLength3 > sideLength1 && sideLength1 + sideLength3 > sideLength2;
         }
 
         public bool IsStringANumber(string input)
         {
-            throw new NotImplementedException();
+            if(input == null || !input.Any())
+            {
+                return false;
+            }
+            return input.Replace("-","").Replace(".","").All(char.IsNumber);
         }
 
         public bool MajorityOfElementsInArrayAreNull(object[] objs)
         {
-            throw new NotImplementedException();
+            return objs.Where(o => o == null).ToArray().Length > objs.Where(o => o != null).ToArray().Length;
         }
 
         public double AverageEvens(int[] numbers)
         {
-            throw new NotImplementedException();
+            if(numbers == null || numbers.Length == 0)
+            {
+                return 0;
+            }
+            int[] evens = numbers.Where(x => x % 2 == 0).ToArray();
+            if(evens.Length == 0)
+            {
+                return 0;
+            }
+            double result = (1.0*evens.Sum()) / evens.Length;
+            return result;
         }
 
         public int Factorial(int number)
         {
-            throw new NotImplementedException();
+            if(number < 0)
+            {
+                throw new ArgumentOutOfRangeException(number.ToString());
+            }
+            int fact = 1;
+            while(number > 1)
+            {
+                fact = fact * number;
+                number = number - 1;
+            }
+            return fact;
         }
     }
 }
